65072dc0c305fdc0d5d263dbf7409ba923265438
speters-github <umpet347@myumanitoba.ca>
2022-11-10 17:20:03 -0600

    Created a quick script to determine the unique (non-blacklist) words, and their associated count, in a single transcript.  It also prints out a bar chart showing said results.

---
+++ word_counts/word_counts.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from transcript_parser.transcript_parser import Parser
+import constants
+
+
+# this is just a scrap file right now to show a concept
+parser = Parser(constants.transcript_en_loc)
+
+clean_transcript_sentences = parser.get_clean_sentences()
+# will also do the original sentences (minus punctuation) when that feature
+# is added to the transcript_parser code.
+
+first_transcript_sentences = clean_transcript_sentences[0]
+
+word_counts = {}
+
+for sentence_list in first_transcript_sentences:
+    for word in sentence_list:
+
+        # initialize the count
+        if word not in word_counts:
+            word_counts[word] = 1
+        else:
+            word_counts[word] += 1
+
+print(f"There are {len(word_counts)} unique non-blacklist words. "
+      "The words, with their associated counts, are:")
+
+unique_words = sorted(word_counts.keys())
+unique_word_counts = []
+
+for word in unique_words:
+    print(f"{word}: {word_counts[word]}")
+    unique_word_counts.append(word_counts[word])
+
+plt.bar(unique_words, unique_word_counts)
+plt.show()
