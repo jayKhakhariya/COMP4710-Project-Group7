b649f444d25e6d42d37dbbadb1063b9e8e0e0aed
speters-github <umpet347@myumanitoba.ca>
2022-12-05 22:49:58 -0600

    Updated the word_counts.py file to count across all talks.

--- counting_things/word_counts.py
+++ counting_things/word_counts.py
@@ -10,18 +10,17 @@ clean_transcript_sentences = parser.get_clean_sentences()
 # will also do the original sentences (minus punctuation) when that feature
 # is added to the transcript_parser code.
 
-first_transcript_sentences = clean_transcript_sentences[0]
-
 word_counts = {}
 
-for sentence_list in first_transcript_sentences:
-    for word in sentence_list:
+for talk in clean_transcript_sentences:
+    for sentence_list in talk:
+        for word in sentence_list:
 
-        # initialize the count
-        if word not in word_counts:
-            word_counts[word] = 1
-        else:
-            word_counts[word] += 1
+            # initialize the count
+            if word not in word_counts:
+                word_counts[word] = 1
+            else:
+                word_counts[word] += 1
 
 print(f"There are {len(word_counts)} unique non-blacklist words. "
       "The words, with their associated counts, are:")
@@ -30,7 +29,8 @@ unique_words = sorted(word_counts.keys())
 unique_word_counts = []
 
 for word in unique_words:
-    print(f"{word}: {word_counts[word]}")
+    if word_counts[word] > 100:
+        print(f"{word}: {word_counts[word]}")
     unique_word_counts.append(word_counts[word])
 
 plt.bar(unique_words, unique_word_counts)
