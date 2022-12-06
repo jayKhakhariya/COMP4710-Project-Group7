f306a290cb005750145e69f9993552ee60b2d381
speters-github <umpet347@myumanitoba.ca>
2022-12-05 22:11:48 -0600

    Initial commit for creating the laughter_avc generating script.

--- counting_things/add_count_occurrence_column.py
+++ counting_things/add_count_occurrence_column.py
@@ -1,5 +1,4 @@
 import csv
-import constants
 from transcript_parser.transcript_parser import FieldNames
 
 
@@ -36,21 +35,3 @@ class AddCountOccurrenceColumn:
         transcript = data_row[FieldNames.transcript].lower()
 
         return transcript.count(self.str_to_count)
-
-
-"""
-This is just a sample to show you that it works, with the string we're 
-counting as \"(laughter)\".  We can remove it later when we know more 
-about what it is that we're going to be doing.
-"""
-laughter_str = "(laughter)"
-new_dataset = AddCountOccurrenceColumn(constants.transcript_en_loc, laughter_str).data_set
-
-"""
-Only printing the first 2 rows, to show the column names, and the count for
-how many occurrences of \"(laughter)\" were found in the first transcript.
-"""
-for i in range(2):
-    print(f"Row {i}")
-    print(new_dataset[i])
-    print()
